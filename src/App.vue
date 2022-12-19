4aff203d593dbd00287cd90c3f1c56d87e21ecc5
zzerx <zzerx@qq.com>
2022-12-19 16:16:25 +0800

    添加公共样式

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,14 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url('./common/base.css');
+@import url('./common/reset.css');
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
+#app{
+  background: var(--color-mian-bg);
+  width: 100vw;
+  height: 100vh;
 }
 </style>

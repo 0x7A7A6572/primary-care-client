3d828a630ee21613bc6fb57165691b311d8cf71a
zzerx <zzerx@qq.com>
2022-12-20 00:07:21 +0800

    基本目录结构

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -6,6 +6,7 @@
     <!-- 通过 class-prefix 指定类名为 yl-icon -->
     <van-icon class-prefix="yl-icon" name="dingwei" />
     <van-icon name="wap-home-o" />
+    <router-link  to="/home/MedicationReminder">进入用药提醒</router-link>
   </div>
 </template>
 
@@ -14,7 +15,7 @@ import ylTitle from "@/components/ylTitle.vue";
 export default {
   name: "HomeView",
   components: {
-    ylTitle,
+    ylTitle
   },
 };
 </script>
---
+++ src/views/UserView/UserView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="user-view">
+    <h1>用户界面</h1>
+  </div>
+</template>
